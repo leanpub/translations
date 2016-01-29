7084e4c506d3a101296ab2bb2911686d88bd6f5a
Len Epp <lenepp@gmail.com>
2016-01-29 15:13:54 -0800

    no need for en.only.yml to be here

--- copy_from_leanpub.sh
+++ copy_from_leanpub.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rm *.yml
 cp -r ../lpub/config/locales/*.yml .
+rm en.only.yml
--- copy_to_leanpub.sh
+++ copy_to_leanpub.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+mv ../lpub/config/locales/en.only.yml ../lpub/config/locales/en.only.yml.noclobber
 rm ../lpub/config/locales/*.yml
+mv ../lpub/config/locales/en.only.yml.noclobber ../lpub/config/locales/en.only.yml
 cp -r *.yml ../lpub/config/locales
